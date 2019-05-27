4a2efd55717a6a8bdc28c3f02d3bb536b00de9bb
Roberto Coscia <45144147+robertocoscia@users.noreply.github.com>
2019-05-27 16:52:46 +0100

    num2.js

---
+++ num2.js
@@ -0,0 +1,55 @@
+
+var headOne= document.querySelector('#hover')
+var headTwo= document.querySelector('#contact')
+var headThree= document.querySelector('#listsocial')
+var headFour= document.querySelector('#summarytitle')
+var headFive= document.querySelector('#education')
+var headSix= document.querySelector('#display-8')
+
+headOne.addEventListener('mouseover', function(){
+  headOne.style.color = 'yellow';
+})
+headOne.addEventListener("mouseout",function(){
+  headOne.textContent= "Roberto Coscia Portfolio";
+  headOne.style.color='black';
+})
+
+headTwo.addEventListener('mouseover', function(){
+  headTwo.style.color = 'yellow';
+})
+headTwo.addEventListener("mouseout",function(){
+  
+  headTwo.style.color='black';
+})
+
+headThree.addEventListener('mouseover', function(){
+  headThree.style.color = 'yellow';
+})
+headThree.addEventListener("mouseout",function(){
+  headThree.textContent= "List of Social Media";
+  headThree.style.color='black';
+})
+
+headFour.addEventListener('mouseover', function(){
+  headFour.style.color = 'yellow';
+})
+headFour.addEventListener("mouseout",function(){
+  headFour.textContent= "Summary of the main Skills";
+  headFour.style.color='black';
+})
+
+headFive.addEventListener('mouseover', function(){
+  headFive.style.color = 'yellow';
+})
+headFive.addEventListener("mouseout",function(){
+  headFive.textContent= "Education";
+  headFive.style.color='black';
+})
+
+headSix.addEventListener('mouseover', function(){
+  headSix.style.color = 'yellow';
+})
+headSix.addEventListener("mouseout",function(){
+  headSix.textContent= "My Projects!";
+  headSix.style.color='black';
+})
